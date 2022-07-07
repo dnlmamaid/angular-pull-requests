1cf8461544c121bf3288eaed0d140284f11c1126
dan <daniel.mamaid@dxc.com>
2022-07-08 00:04:22 +0800

    updates material components

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
-  imports: [MatCardModule],
-  exports: [MatCardModule],
+  imports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
