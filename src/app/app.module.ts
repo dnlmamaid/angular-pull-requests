25fc0d43fed1ef1663d09b495c7c370deea22bfd
dan <daniel.mamaid@dxc.com>
2022-07-08 00:06:23 +0800

    updates app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/modules/material.module';
+import { CommonModule } from '@angular/common';
+import { PullRequestDetailsComponent } from './components/pull-requests/pull-request-details/pull-request-details.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PullRequestsComponent } from './components/pull-requests/pull-requests.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    PullRequestsComponent,
+    PullRequestDetailsComponent,
+  ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    CommonModule,
     HttpClientModule,
     MaterialModule,
+  ],
+  exports:[
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent],
